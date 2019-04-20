d55f981f662516786b62ddf7d7e62cf8a3dc2304
Artem Liger <47827465+artemliger@users.noreply.github.com>
2019-04-20 19:30:54 +0200

    Add files via upload
    update files

--- src/js/main.js
+++ src/js/main.js
@@ -13,40 +13,123 @@ window.onload = function(){
 
     for ( let i = 0; i < categoryItem.length; i++) {
         categoryItem[i].addEventListener('mouseenter', () => {
-            hoverItem[i].classList.remove('hidden');
+            hoverItem[i].classList.remove('clear');
         });
 
         categoryItem[i].addEventListener('mouseleave', () => {
-            hoverItem[i].classList.add('hidden');
+            hoverItem[i].classList.add('clear');
         });
     }
 
-    $('.show').slick({
-        arrows: true,
+
+
+    let $show = $('.show');
+    let showSettings = {
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        speed: 1000,
+        responsive: [
+            {
+                breakpoint: 99999,
+                settings: "unslick"
+            },{
+
+                breakpoint: 750,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },{
+                breakpoint: 600,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },{
+                breakpoint: 380,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    };
+
+    $show.slick(showSettings);
+
+    $(window).on('resize', function() {
+        if (!$show.hasClass('slick-initialized')) {
+            return $show.slick(showSettings);
+        }
+    });
+    /*
+    $('.show').slick({
+        mobileFirst: true,
+        infinite: false,
         autoplay: true,
         autoplaySpeed: 3000,
         speed: 1000,
         slidesToShow: 3,
         slidesToScroll: 3,
-        mobileFirst: true,
         responsive: [
             {
-                breakpoint: 960,
-                settings: 'unslick'
+                breakpoint: 1290,
+                settings: "unslick"
             },
             {
-                breakpoint: 950,
-                settings: 'slick'
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 380,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
             }
         ]
-    });
+    });*/
 
     $('.burger-btn').on("click", function() {
-        $('.header-top__bar').slideToggle('slow');
+        $('.bar-list').slideToggle('slow');
     });
 
 
+    $(document).ready(function(){
+
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 150) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+
+        $('.scrollup').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+
+    });
 
 
 
